c00adeea1e4552ad513c9bc018df4bc2185b6db3
ardhiseptianalqohar <ardhiseptianalqohar@gmail.com>
2022-11-17 05:11:52 +0700

    Initial-Commmit

--- bin/Function-optional-parameter/main.dart
+++ bin/Function-optional-parameter/main.dart
@@ -5,3 +5,21 @@ memiliki nama, dan bertugas untuk melakukan tugas spesifik tertentu.
 fungsi dapat(OPTIONAL) mengembalikan nilai pada pemanggilnya. Jika tidak
 mengembalikan nilai apapun, maka tipe kembaliannya adalah void
 */
+
+void main(List<String> args) {
+  cetakPerkalian(5,5);
+  cetakTambah(10, 10);
+  cetakPembagian(20, 5);
+}
+
+void cetakPerkalian(int angka1, int angka2) {
+  print('hasil = ${angka1 * angka2}');
+}
+
+void cetakTambah(int angka1, int angka2) {
+  print('Hasil = ${angka1 + angka2}');
+}
+
+void cetakPembagian(double angka1, double angka2) {
+  print('Hasil = ${angka1 / angka2}');
+}
