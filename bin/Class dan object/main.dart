5a32ea5e0d440d47709b55ef6c9aeab77cf1aa05
ardhiseptianalqohar <ardhiseptianalqohar@gmail.com>
2022-11-17 19:47:39 +0700

    Initial-commit

--- bin/Class dan object/main.dart
+++
@@ -1,6 +0,0 @@
-/* 
-APA ITU PEMROGRAMAN BERBASIS OBJECT(OOP)..?
--Sebuah cara/konsep/sudut pandang mengenai bagaimana membuat sebuah sistem
--yang terdiri dari satu/object yang saling berkaitan.
-*/
-
---
+++ bin/Class-dan-object/main.dart
@@ -0,0 +1,21 @@
+/* 
+APA ITU PEMROGRAMAN BERBASIS OBJECT(OOP)..?
+-Sebuah cara/konsep/sudut pandang mengenai bagaimana membuat sebuah sistem
+-yang terdiri dari satu/object yang saling berkaitan.
+
+APA ITU CLASS ?
+-Rancangan dari sebuah object.
+-Anggota (member) dari sebuah class :
+-Field : Apa yang dimiliki class.
+-Method/Fungsi : Apa yang dapat dilakukan oleh class.
+
+APA ITU OBJECT ?
+-Realisasi/bentuk nyata dari sebuah class.
+-Identifier: Nama panggilan/nicname/petunjuk/pointer yang kita gunakan untuk
+-mengakses/menggunakan/memanggil object yg kita miliki.
+-Ketika sebuah identifier tidak menunjuk pada objek apapun, maka yg ia tunjuk adalah Null.
+
+CLASS & OBJECT = Dalam sebuah sistem, sebuah class hanya ada satu. 
+-Sedangkan objek dari class tersebut dapat berjumlah lebih dari satu.
+*/
+
